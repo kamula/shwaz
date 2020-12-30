c026ca130d05ae4d528e0fdf31ef5f5a47fe7f67
kamula <kamulaisaac@gmail.com>
2020-12-30 16:26:59 +0300

    buffer update

--- app.js
+++ app.js
@@ -5,10 +5,18 @@ const server = http.createServer((req, res) => {
     const url = req.url;
     const method = req.method
         //console.log(req.url, req.method, req.headers);
-    if (url === '/') {
-        fs.writeFile('message.txt', 'Dummy', (err) => {
-            console.log("file created")
+    if (url === '/' && method === "POST") {
+        const body = []
+        req.on('data', chunck => {
+            console.log(chunck)
+            body.push(chunck)
+            const parsedbody = Buffer.concat(body).toString()
+            fs.writeFile('message.txt', parsedbody, (err) => {
+                console.log(parsedbody)
+            })
+
         })
+
 
     }
 
