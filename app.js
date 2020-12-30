3fcc89b79ed21d81dcfde4fcddeed02f33292622
kamula <kamulaisaac@gmail.com>
2020-12-30 10:23:57 +0300

    asych file creation

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const http = require("http");
+const fs = require('fs')
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
-    res.setHeader("Content-Type", "text/html");
-    res.end();
+    const url = req.url;
+    const method = req.method
+        //console.log(req.url, req.method, req.headers);
+    if (url === '/') {
+        fs.writeFile('message.txt', 'Dummy', (err) => {
+            console.log("file created")
+        })
+
+    }
+
 });
 
 server.listen(3000);
