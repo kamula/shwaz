f26d0c830541664d754d0b208e5533d2f690f150
kamula <kamulaisaac@gmail.com>
2020-12-31 12:32:11 +0300

    express 101

--- app.js
+++ app.js
@@ -1,25 +1,4 @@
 const http = require("http");
-const fs = require('fs')
+const express = require("express");
 
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    const method = req.method
-        //console.log(req.url, req.method, req.headers);
-    if (url === '/' && method === "POST") {
-        const body = []
-        req.on('data', chunck => {
-            console.log(chunck)
-            body.push(chunck)
-            const parsedbody = Buffer.concat(body).toString()
-            fs.writeFile('message.txt', parsedbody, (err) => {
-                console.log(parsedbody)
-            })
-
-        })
-
-
-    }
-
-});
-
-server.listen(3000);
+const app = express();
