b89af2568c624ab1353318304f2682b217face61
kamula <kamulaisaac@gmail.com>
2021-01-01 11:43:40 +0300

    project routes

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const http = require("http");
 const express = require("express");
+const bodyParser = require("body-parser")
+
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
 
 const app = express();
-app.use((req, res, next) => {
-    console.log("in the middleware");
-    next();
-});
-app.use((req, res, next) => {
-    console.log("in another middleware");
-    res.send('<h1>Hello from express</h1>');
-});
-const server = http.createServer(app);
 
-server.listen(3000);
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use("/admin", adminRoutes)
+app.use(shopRoutes)
+
+app.use((req, res, next) => {
+    res.status(404).send("<h1>Page not found</h1>");
+})
+
+app.listen(3000);
+// const server = http.createServer(app);
+
+// server.listen(3000);
