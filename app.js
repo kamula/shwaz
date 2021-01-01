3b6449394ec53a1fb8b075c79c73368772e6ae49
kamula <kamulaisaac@gmail.com>
2021-01-01 13:30:37 +0300

    render file in requests

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 const http = require("http");
+const path = require("path");
 const express = require("express");
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
 
-const adminRoutes = require('./routes/admin')
-const shopRoutes = require('./routes/shop')
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use("/admin", adminRoutes)
-app.use(shopRoutes)
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).send("<h1>Page not found</h1>");
-})
+    res.status(404).sendFile(path.join(__dirname, './', "views", "404.html"));
+});
 
 app.listen(3000);
 // const server = http.createServer(app);
--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const path = require('path')
 const router = express.Router();
+
+const rootDir = require('../utils/path')
 router.get("/add-product", (req, res, next) => {
-    console.log("in another middleware");
-    res.send(
-        '<form action ="/admin/product" method = "POST"><input type="text" name = "title"><button type="submit">Add product</button></form>'
-    );
+    console.log(req.body)
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
+
 });
 router.post("/product", (req, res, next) => {
     console.log(req.body);
