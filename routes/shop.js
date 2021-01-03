df5a068ba7ee95c13f8d87d86cccbc5c833606ad
kamula <kamulaisaac@gmail.com>
2021-01-03 23:14:12 +0300

    products & routes modified

--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,12 @@
 const express = require("express")
 const path = require('path')
 const rootDir = require('../utils/path')
+const admindata = require("./admin");
 
 const router = express.Router()
 
 router.get("/", (req, res, next) => {
+    console.log(admindata.products)
     res.sendFile(path.join(rootDir, 'views', 'shop.html'))
 });
 module.exports = router
