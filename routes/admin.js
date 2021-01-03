b9513596fc9807db17f95cb213b5cfe89eebd11f
kamula <kamulaisaac@gmail.com>
2021-01-03 23:12:31 +0300

    products array

--- routes/admin.js
+++ routes/admin.js
@@ -2,14 +2,17 @@ const express = require("express");
 const path = require('path')
 const router = express.Router();
 
+let product = []
 const rootDir = require('../utils/path')
 router.get("/add-product", (req, res, next) => {
     console.log(req.body)
     res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
 
 });
-router.post("/product", (req, res, next) => {
+router.post("/add-product", (req, res, next) => {
+    product.push({ title: req.body.title })
     console.log(req.body);
     res.redirect("/");
 });
-module.exports = router;
+exports.routes = router;
+exports.products = product;
