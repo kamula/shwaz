82f1926124aee629eee2667d20815cf0b2c874a6
kamula <kamulaisaac@gmail.com>
2021-01-04 23:07:04 +0300

    render pug file

--- routes/shop.js
+++ routes/shop.js
@@ -6,7 +6,6 @@ const admindata = require("./admin");
 const router = express.Router()
 
 router.get("/", (req, res, next) => {
-    console.log(admindata.products)
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'))
+    res.render("shop")
 });
 module.exports = router
