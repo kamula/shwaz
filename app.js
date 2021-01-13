be445d666f130e048a8ad2f78b2686fd3922278e
kamula <kamulaisaac@gmail.com>
2021-01-13 22:03:37 +0300

    main layout

--- app.js
+++ app.js
@@ -2,13 +2,20 @@ const http = require("http");
 const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
-const expressHbs = require('express-handlebars')
+const expressHbs = require("express-handlebars");
 
 const admindata = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 const app = express();
-app.engine('hbs', expressHbs());
+app.engine(
+    "hbs",
+    expressHbs({
+        extname: "hbs",
+        defaultLayout: "main-layout",
+        layoutsDir: "views/layouts",
+    })
+);
 app.set("view engine", "hbs");
 app.set("views", "views");
 
@@ -18,7 +25,7 @@ app.use("/admin", admindata.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.render("404", { pageTitle: "Page Not Found" })
+    res.render("404", { pageTitle: "Page Not Found" });
 });
 
 app.listen(3000);
